eba137ee9191d1b5e6bbf78863269ad1565a3ad2
szerneknicolas-ux <szerneknicolas@gmail.com>
2026-06-19 00:43:08 -0300

    Add files via upload

---
+++ remover_fundo.py
@@ -0,0 +1,11 @@
+﻿from rembg import remove
+
+pasta = r'C:\Users\HP\OneDrive\Área de Trabalho\Projeto do meu pai\public_html'
+
+for nome in ['homem triste', 'homem feliz']:
+    with open(f'{pasta}\\{nome}.png', 'rb') as f:
+        dados = f.read()
+    resultado = remove(dados)
+    with open(f'{pasta}\\{nome}-sem-fundo.png', 'wb') as f:
+        f.write(resultado)
+    print(f'{nome} processado com sucesso!')
